1cd6c7f682695073bf4191157981b0d3bad6e679
Ahimsa <ahimsa@Ahimsas-Mac-mini.local>
2026-05-25 17:11:19 -0700

    Set nvim encoding

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- Set global encoding to UTF-8
+vim.opt.encoding = "utf-8"
+
+-- Set file encoding for the current buffer
+vim.opt.fileencoding = "utf-8"
