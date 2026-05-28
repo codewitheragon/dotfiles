e47f246c756f77b107a013de44a98e18b8228a3a
Ahimsa <ahimsa@Ahimsas-Mac-mini.local>
2026-05-28 16:59:50 -0700

    Minor keymap changes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Better indentation: stay in visual mode
+vim.keymap.set("v", "<", "<gv", { desc = "Indent out" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent in" })
